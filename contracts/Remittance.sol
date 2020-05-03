28c3eec22b10f46d99d71f740ecfe3639854db8e
marie <malou@Du.local>
2020-05-03 19:26:32 +0200

    made makePasswords() into a view function

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -8,22 +8,22 @@ contract Remittance is Killable {
     mapping (address => uint) public balances;
     event LogAmountSent(address indexed from, address indexed carol, uint256 amount);
     event LogAmountWithdrawn(address indexed from, uint256 amount);
-    bytes32 public alicesPassword1;
-    bytes32 public alicesPassword2;
     using SafeMath for uint256; 
-
+    bytes32 hashedPassword1; 
+    bytes32 hashedPassword2;
     constructor() public {} 
 
-    function makePasswords(bytes32 password1, bytes32 password2) public whenRunning whenAlive onlyOwner returns(bytes32 _alicesPassword1, bytes32 _alicesPassword2) {
-        alicesPassword1 = keccak256(abi.encodePacked(password1));
-        alicesPassword2 = keccak256(abi.encodePacked(password2));
-
-        return (alicesPassword1, alicesPassword2);
+    function makePasswords(bytes32 plainPassword1, bytes32 plainPassword2) public view whenRunning whenAlive onlyOwner returns(bytes32 _hashedPassword1, bytes32 _hashedPassword2) {
+        return (keccak256(abi.encodePacked(plainPassword1)), keccak256(abi.encodePacked(plainPassword2)));
     }
 
-    function remit(address carol) public payable whenRunning whenAlive onlyOwner returns(bool success) {
+    function remit(address carol, bytes32 _hashedPassword1, bytes32 _hashedPassword2) public payable whenRunning whenAlive onlyOwner returns(bool success) {
         require(msg.value != 0, "You need to send value.");
         require(carol != address(0x0), "You need to pass a valid address.");
+
+        hashedPassword1 = _hashedPassword1;
+        hashedPassword2 = _hashedPassword2; 
+
         uint256 amount = msg.value;
 
         balances[carol] = balances[carol].add(amount);
@@ -32,13 +32,15 @@ contract Remittance is Killable {
         return true;
     }
 
-    function withdraw(bytes32 passwordBob, bytes32 passwordCarol) public whenRunning whenAlive returns(bool success) {
+    function withdraw(bytes32 plainPasswordBob, bytes32 plainPasswordCarol) public whenRunning whenAlive returns(bool success) {
         uint256 toWithdraw = balances[msg.sender];
         require(toWithdraw != 0, "You don't have funds to withdraw!");
-        bytes32 _passwordBob = keccak256(abi.encodePacked(passwordBob)); 
-        bytes32 _passwordCarol = keccak256(abi.encodePacked(passwordCarol));
-        require(_passwordBob == alicesPassword1, "Bobs password is incorrect!");
-        require(_passwordCarol == alicesPassword2, "Carols password is incorrect!");
+
+        bytes32 hashedPasswordBob = keccak256(abi.encodePacked(plainPasswordBob)); 
+        bytes32 hashedPasswordCarol = keccak256(abi.encodePacked(plainPasswordCarol));
+        
+        require(hashedPasswordBob == hashedPassword1, "Bobs password is incorrect!");
+        require(hashedPasswordCarol == hashedPassword2, "Carols password is incorrect!");
         
         balances[msg.sender] = 0;
         emit LogAmountWithdrawn(msg.sender, toWithdraw);
@@ -46,9 +48,4 @@ contract Remittance is Killable {
         (success, ) = msg.sender.call.value(toWithdraw)("");
         require(success, "No value was transfered to your account.");
     }
-
-    function getBalance(address _key) public view returns(uint) {
-        return balances[_key];
-    }
-
 }
