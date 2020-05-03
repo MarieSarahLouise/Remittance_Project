0463b3e632d3800800b76d0da0ca57dca88879d5
marie <malou@Du.local>
2020-05-03 19:25:47 +0200

    changed the tests

--- test/remittance.js
+++ test/remittance.js
@@ -2,7 +2,7 @@ const Remittance = artifacts.require("Remittance");
 const assert = require("assert");
 const chai = require("chai");
 const truffleAssert = require("truffle-assertions");
-const { toWei, toBN } = web3.utils;
+const { toWei, toBN, sha3 } = web3.utils;
 
 
 contract("Remittance", (accounts) => {
@@ -15,20 +15,20 @@ contract("Remittance", (accounts) => {
 
     it("Remit should not work if you send no value.", async function(){
         await truffleAssert.reverts(
-            contractInstance.remit(carol, { from: alice, value: 0 }), 
+            contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: 0 }), 
             "You need to send value.")
     });
 
     it("Only the owner can call remit.", async function(){
         const amount = toWei(toBN("1"), "Gwei");
         await truffleAssert.reverts(
-            contractInstance.remit(carol, { from: notOwner, value: amount }), 
+            contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: notOwner, value: amount }), 
             "Only the owner can call this function.")
     });
 
     it("Remit emits the proper event", async function(){
         const amount = toWei(toBN("1"), "Gwei");
-        const remitObj = await contractInstance.remit(carol, { from: alice, value: amount});
+        const remitObj = await contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: amount});
         const { logs } = remitObj;
         assert.strictEqual(logs.length, 1);
         const remitEvent = remitObj.logs[0];
@@ -40,25 +40,24 @@ contract("Remittance", (accounts) => {
 
     it("Carols internal balance after remit should be right.", async function() {
         const amount = toWei(toBN("1"), "Gwei");
-        await contractInstance.remit(carol, { from: alice, value: amount});
-        const balanceAfter = await contractInstance.getBalance(carol);
+        await contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: amount});
+        const balanceAfter = await contractInstance.balances(carol);
         assert.strictEqual(balanceAfter.toString(), amount.toString());
     });
 
     it("Carols internal balance after withdraw should be 0.", async function(){
         const amount = toWei(toBN("1"), "Gwei");
         await contractInstance.makePasswords("0x123", "0x456", { from: alice });
-        await contractInstance.remit(carol, { from: alice, value: amount });
-        
+        await contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: amount });
         await contractInstance.withdraw("0x123", "0x456", { from: carol });
-        const carolsBalance = await contractInstance.getBalance(carol);
+        const carolsBalance = await contractInstance.balances(carol);
         assert.strictEqual(carolsBalance.toString(), "0");
     });
 
     it("Withdraw should emit the expected event.", async function() {
         const amount = toWei(toBN("1"), "Gwei");
         await contractInstance.makePasswords("0x123", "0x456", { from: alice });
-        await contractInstance.remit(carol, { from: alice, value: amount });
+        await contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: amount });
         const withdrawObj = await contractInstance.withdraw("0x123", "0x456", { from: carol });
         const { logs } = withdrawObj;
         assert.strictEqual(logs.length, 1);
@@ -71,7 +70,7 @@ contract("Remittance", (accounts) => {
     it("Withdraw should not work if Bobs password is wrong.", async function(){
         const amount = toWei(toBN("1"), "Gwei");
         await contractInstance.makePasswords("0x123", "0x456", { from: alice });
-        await contractInstance.remit(carol, { from: alice, value: amount });
+        await contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: amount });
         await truffleAssert.reverts(
             contractInstance.withdraw("0x321", "0x456", { from: carol }), 
             "Bobs password is incorrect!");
@@ -80,7 +79,7 @@ contract("Remittance", (accounts) => {
     it("Withdraw should not work if Carols password is wrong.", async function(){
         const amount = toWei(toBN("1"), "Gwei");
         await contractInstance.makePasswords("0x123", "0x456", { from: alice });
-        await contractInstance.remit(carol, { from: alice, value: amount });
+        await contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: amount });
         await truffleAssert.reverts(
             contractInstance.withdraw("0x123", "0x654", { from : carol}), 
             "Carols password is incorrect!");
@@ -96,19 +95,18 @@ contract("Remittance", (accounts) => {
     it("Carols balance should not be reset after each remit.", async function(){
         const amount1 = toWei(toBN("1"), "Gwei");
         const amount2 = toWei(toBN("2"), "Gwei");
-        await contractInstance.remit(carol, { from: alice, value: amount1 });
-        await contractInstance.remit(carol, { from: alice, value: amount2 });
-        const carolsBalance = await contractInstance.getBalance(carol);
+        await contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: amount1 });
+        await contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: amount2 });
+        const carolsBalance = await contractInstance.balances(carol);
 
         assert.strictEqual(carolsBalance.toString(), toBN(amount1).add(toBN(amount2)).toString());
-        
     });
 
     it("Carols balance after withdraw should be right.", async function(){
         const amount = toWei(toBN("1"), "Gwei");
         const balanceBefore = await web3.eth.getBalance(carol);
         await contractInstance.makePasswords("0x123", "0x456", { from: alice });
-        await contractInstance.remit(carol, { from: alice, value: amount });
+        await contractInstance.remit(carol, sha3("0x123"), sha3("0x456"), { from: alice, value: amount });
         const txObj = await contractInstance.withdraw("0x123", "0x456", { from: carol })
         const tx = await web3.eth.getTransaction(txObj.tx);
         const receipt = txObj.receipt;
